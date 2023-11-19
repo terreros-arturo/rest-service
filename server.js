81d474839a9e26398ef8458783b759ad90a4d092
Arturo <=>
2023-11-18 21:47:00 -0600

    middleware basicos para errores y 404

--- server.js
+++ server.js
@@ -26,10 +26,26 @@ app.post('/', (req, res) => {
 
     console.log(`${tag} iniciando`, req.body);
 
+    //error provocado
+    let val = "dasd";
+    console.log(`${tag} valor calculado ${JSON.parse(val)}`);
+
     res.status(200).send({
         message: 'recurso actualizado',
         recibido: req.body
     });
+});
+
+//capturar una excepcion no manejada
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Ocurrio algun error, consulte con su administrador');
+});
+
+
+//Cualquier servicio no existente
+app.use(function(req, res, next) {
+    res.status(404).send('Recurso no encontrado, favor de verificarlo');
 });
 
 
