b83440506c19912b9729aa41278ad5d888aec93d
Yessica Diaz <yuyinia@gmail.com>
2023-11-18 21:19:53 -0600

    version inicial

---
+++ server.js
@@ -0,0 +1,28 @@
+const PORT = 8000;
+
+const express = require ('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.patch('/patch', (req, res) => {
+    const tag = 'patchService';
+
+    console.log(`${tag} iniciando`, req.body);
+
+    res.status(200).send({
+        message: 'recurso actualizado'
+    });
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Aplicacion iniciada en puerto ${PORT}`);
+
+});
