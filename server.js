3fa217dd26dee067bfc5c5726b425e72176fbb07
Arturo Terreros <aterreros@develsuit.com>
2023-12-14 13:34:26 -0600

    upload files

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const PORT = 8000;
 
+const path = require('path');
+const fs = require('fs');
+
 const express = require ('express');
 const app = express();
+
+const multipart = require('connect-multiparty');
+const mdUpload = multipart({uploadDir: __dirname + path.sep + 'tmp' + path.sep});
 
 app.use(express.json());
 
@@ -39,6 +45,57 @@ app.post('/', (req, res) => {
     });
 });
 
+
+app.post('/file', mdUpload, (req, res) => {
+    const tag = 'saveFile';
+    console.log(`${tag} guardando archivo`, req.body);
+
+    if (!req.files) {
+        return res.status(400).send({
+            status: 'error',
+            message: 'No se recibio archivo'
+        });
+    }
+
+    //console.log(`${tag} file`, req.files.file);
+
+    let filePath = req.files.file.path;
+    //filePath = __dirname + path.sep + filePath;
+
+    const fileName = req.files.file.name;
+    const extFile = path.extname(fileName);
+
+    //console.log(`${tag} fileName: '${fileName}', extFile: '${extFile}' `);
+
+    //Ruta para el archivo
+    const newPathDir = `${__dirname + path.sep + 'upload' + path.sep}`;
+    //const newPath = `${newPathDir + id_nodo.replace(/\s/g,'-') + extFile}`;
+    const newPath = `${newPathDir + fileName}`;
+    
+    //console.log(`${tag} newPath '${newPath}'`);
+
+    try{
+        //Se crea el arbol de directorios si no existe
+        if (!fs.existsSync(newPathDir)) {
+            fs.mkdirSync(newPathDir, {recursive:true});
+        }
+
+        //Se mueve de ruta el archivo cargado
+        fs.renameSync(filePath, newPath);
+
+        let id_file = Math.random() * 1000;
+        res.status(200).send({
+            success: 'true',
+            id_file: id_file
+        });
+    }catch(error){
+        console.log(`${tag} error producido al cargar archivo`, error);
+        res.status(500).send({'message': error});
+    };
+});
+
+
+
 //status http 500
 app.use((err, req, res, next) => {
     console.error(err.stack);
@@ -55,5 +112,4 @@ app.use(function(req, res, next) {
 
 app.listen(PORT, () => {
     console.log(`Aplicacion iniciada en puerto ${PORT}`);
-
 });
